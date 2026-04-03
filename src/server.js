29b344092fbd84782387acbcd50c50166a63c497
Sakshi <sakshiparashar2910@gmail.com>
2026-04-03 10:01:11 +0530

    feat(security): add rate-limiter middleware

--- src/server.js
+++ src/server.js
@@ -5,16 +5,18 @@ const {connectDB} = require('./config/db')
 const authRouter = require('./routes/auth-routes');
 const userRouter = require('./routes/user-routes');
 const transactionRouter = require('./routes/transaction-routes');
-const dashboardRouter = require('./routes/dashboard-route');
+const dashboardRouter = require('./routes/dashboard-routes');
+const limiter = require('./middleware/rate-limiter');
 
 const app = express();
 connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(limiter);
 app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
-app.use('/api/transaction', transactionRouter);
+app.use('/api/users', userRouter);
+app.use('/api/transactions', transactionRouter);
 app.use('/api/dashboard', dashboardRouter);
 
 app.listen(PORT, () => {
